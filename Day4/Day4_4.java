e9801464c75cfc528adc9a07e9db78be0cfc44bf
SrishtiChaurasia-29 <85130438+SrishtiChaurasia-29@users.noreply.github.com>
2023-07-09 00:49:53 +0530

    Day4

---
+++ Day4/Day4_4.java
@@ -0,0 +1,18 @@
+int maxLen(int arr[], int n)
+    {
+        HashMap<Integer, Integer> h = new HashMap<>();
+        int cursum = 0;
+        int len = 0;
+        for(int i=0; i<n; i++){
+            cursum+=arr[i];
+            if(cursum==0) len = i+1;
+            
+            if(!h.containsKey(cursum)){ //if sum repeats don't add
+                h.put(cursum, i);
+            }
+            if(h.containsKey(cursum)){  
+                len = Math.max(len, i-h.get(cursum));
+            }
+        }
+        return len;
+    }
