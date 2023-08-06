ed1273cef66b6d4242095a99f01afcd0a84ccf68
SrishtiChaurasia-29 <85130438+SrishtiChaurasia-29@users.noreply.github.com>
2023-08-06 17:33:29 +0530

    Add files via upload

---
+++ Day15/15_1.java
@@ -0,0 +1,40 @@
+class Solution {
+    static int[] bellman_ford(int V, ArrayList<ArrayList<Integer>> edges, int S) {
+        // Write your code here
+        int [] dist=new int[V];
+        Arrays.fill(dist,(int)1e8);
+        
+        dist[S]=0;
+        
+        for(int i=0;i<V-1;i++)
+        {
+            for(ArrayList<Integer> list:edges)
+            {
+                int u=list.get(0);
+                int v=list.get(1);
+                int wt=list.get(2);
+                
+                if(dist[u] != 1e8 && dist[u]+wt < dist[v])
+                {
+                    dist[v]=dist[u]+wt;
+                }
+            }
+        }
+        
+           for(ArrayList<Integer> list:edges)
+            {
+                int u=list.get(0);
+                int v=list.get(1);
+                int wt=list.get(2);
+                
+                if(dist[u] != 1e8 && dist[u]+wt < dist[v])
+                {
+                    int [] temp=new int[1];
+                    temp[0]=-1;
+                    return temp;
+                }
+            }
+            
+            return dist;
+    }
+}
